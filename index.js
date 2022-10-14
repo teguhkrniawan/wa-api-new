1984cd48d17a5a8f863f9a15fe4d4af17070babc
teguhkrniawan <teguhkrniawan@gmail.com>
2022-10-14 15:00:57 +0700

    udah munculin scan wa

--- index.js
+++ index.js
@@ -1,67 +1,93 @@
-
 // import whtasapp-api js
-const { Client, LegacySessionAuth,  LocalAuth } = require('whatsapp-web.js');
+const { Client, LegacySessionAuth, LocalAuth } = require("whatsapp-web.js");
 // import terminal qr code
-const qrcode = require('qrcode-terminal');
+// const qrcode = require("qrcode-terminal");
+const qrcode = require("qrcode");
 // import untuk write file
-const fs = require('fs');
+const fs = require("fs");
+// import express
+const express = require("express");
+// import io
+const socketIO = require("socket.io");
+// http
+const http = require("http");
 
+const app = express();
+const server = http.createServer(app);
 
-const SESSION_FILE_PATH = '/whatsapp-session.json';
-let sessionCfg;
-
-if(fs.existsSync(SESSION_FILE_PATH)) {
-    sessionCfg = require(SESSION_FILE_PATH);
-}
+// connect ke socket sever
+const io = socketIO(server);
 
 const client = new Client({
-    puppeteer: {
-        headless: true
-    },
-    authStrategy: new LocalAuth({
-        clientId: "client-one"
-    })
+  puppeteer: {
+    headless: true,
+  },
+  authStrategy: new LocalAuth({
+    clientId: "client-one",
+  }),
 });
 
-client.initialize(); 
+client.initialize();
 
 // apabila masih loading screen
-client.on('loading_screen', (percent, message) => {
-    console.log('LOADING SCREEN', percent, message);
-});
- 
-// apabila qr code telah diterima
-client.on('qr', (qr) => {
-    // console.log("QR Code yang diterima : ", qr);
-    qrcode.generate(qr);
+client.on("loading_screen", (percent, message) => {
+  console.log("LOADING SCREEN", percent, message);
 });
 
 // apabila sudah terautentikasi
-client.on('authenticated', (session) => {
-    console.log('AUTHENTICATED', session);
-    sessionCfg = session;
+client.on("authenticated", (session) => {
+  console.log("AUTHENTICATED");
+  sessionCfg = session;
 });
 
-client.on('auth_failure', msg => {
-    // Fired if session restore was unsuccessful
-    console.error('AUTHENTICATION FAILURE', msg);
+client.on("auth_failure", (msg) => {
+  // Fired if session r estore was unsuccessful
+  console.error("AUTHENTICATION FAILURE", msg);
 });
-
-
-// apabila sudah ready 
-client.on('ready', () => {
-    console.log('Client is ready ...');
-})
 
 // apabila org ngirim pesan kekita, maka akan kita balas...
-client.on('message', msg => {
-    if(msg.body){
-        msg.reply('jangan balas wa ini, saya adalah BOT');
-    }
-})
-
-// apabila logout
-client.on('disconnected', (reason) => {
-    console.log('Client was logged out', reason);
+client.on("message", (msg) => {
+  if (msg.body) {
+    msg.reply("jangan balas wa ini, saya adalah BOT");
+  }
 });
 
+// apabila logout
+client.on("disconnected", (reason) => {
+  console.log("Client was logged out", reason);
+});
+
+// routing app
+app.get("/", (req, res) => {
+  // res.status(200).json({
+  //     status: true,
+  //     message: "Hello World",
+  //     root: __dirname
+  // })
+  res.sendFile("index.html", {
+    root: __dirname,
+  });
+});
+
+// socket io
+io.on("connection", function (socket) {
+  socket.emit("message", "Connecting....");
+
+  // apabila sudah ready
+  client.on("ready", () => {
+    socket.emit("message", "Whatsapp is ready!");
+  });
+
+  // apabila qr code telah diterima
+  client.on("qr", (qr) => {
+    qrcode.toDataURL(qr, (err, url) => {
+      socket.emit("qrurl", url);
+      socket.emit("message", "Scan QR for use whatsapp!");
+    });
+  });
+});
+
+// jalankan aplikasi
+server.listen(3001, function () {
+  console.log("App running on port 3001...");
+});
